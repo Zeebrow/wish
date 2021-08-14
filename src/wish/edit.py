a28da672e661a3b6c828da60a2fe7909fd8df72c
Zeebrow <mzborowski@yahoo.com>
2021-08-14 17:42:55 -0400

    save

--- src/wish/edit.py
+++ src/wish/edit.py
@@ -1,32 +1,45 @@
 import logging
 import click
 import constants as C
+import ls
 
 logger = logging.getLogger(__name__)
 
 @click.command()
 @click.argument('wish')
-def get_wish(wish: str, quiet=True):
+def edit_wish(wish: str, quiet=True):
     """
     Prints the markdown block associated with the specified wish.
     Exits 0 on success and 1 otherwise.
     """
-    if wish not in _get_wishes():
+    print('edit wish')
+    if wish not in ls._get_wishes():
         logger.warning(f"Wish not found: {wish}")
         return None
-#   try:
-#       os.stat(C.skelpath + wish)
-#       logger.debug(f"Stat '{C.skelpath + wish}'")
-#   except Exception as e:
-#       logger.watning(f"Could not stat '{C.skelpath + wish}': {e}")
+    block=''
     with open(C.wishlist, 'r') as wl:
-        _print_output=False
+        append_output=False
         for line in wl:
-            if line.startswith(f'## ') and _print_output:
-                _print_output = False
-            if _print_output:
-                print(line.strip())
-                # print(''.join(line.strip().split(' ')[1:]))
             if line.startswith(f'## {wish}'):
-                _print_output = True
+                print('match')
+                append_output = True
+                # block = block.join(line.strip())
+                continue
+            if line.startswith(f'## ') and append_output:
+                append_output = False
+            if append_output:
                 print(line.strip())
+                block = block.join(line.strip())
+    print(block)
+    block = click.edit(block, require_save=True, extension='.md')
+    if not block:
+        logger.warning(f"Wish '{wish}' not created - changes were not saved.")
+        return None
+    if debug:
+        fname = tempfile.mkstemp(text=True, prefix=f"{wish}-", suffix=".tmp")
+        print(f"fname: {fname}")
+        with open(fname[1], 'w') as f:
+            f.write(dedent(block))
+    print('---')
+    print(block)
+
